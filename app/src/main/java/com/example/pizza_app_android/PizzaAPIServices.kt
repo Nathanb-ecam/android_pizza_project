5d0a688eacc71156e014808bdf93a800a7dfb3da
Nathanb-hub <nath.buchin@gmail.com>
2023-03-22 20:58:51 +0100

    fixed api get

--- app/src/main/java/com/example/pizza_app_android/PizzaAPIServices.kt
+++ app/src/main/java/com/example/pizza_app_android/PizzaAPIServices.kt
@@ -6,8 +6,9 @@ import retrofit2.http.GET
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
+import retrofit2.http.Path
 
-private const val BASE_URL = "http://localhost:3000/api/"
+private const val BASE_URL = "http://10.0.2.2:3000/api/"
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
@@ -16,8 +17,12 @@ private val retrofit = Retrofit.Builder()
 
 
 interface PizzaApiServices  {
-    @GET("pizzas")//quotes.json
+    @GET("pizzas")
     suspend fun getPizzas():MutableMap<String,List<Pizza>>
+
+
+    @GET("pizza/{pizza_id}")
+    suspend fun getPizzaById(@Path("pizza_id") searchById:String):Pizza
 }
 
 // singleton to limit high ressources use
